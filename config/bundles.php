28948e2655e9e698066dd2eee24e9e992103df57
FOTSO TEMEU Samuel Daryl <temmodaryl317@gmail.com>
2025-06-05 16:37:58 +0200

    L'ajout de pagination

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
